31460eac17572dc7f5aa8bde4d4e14f0da67cdea
Diego Ceron <diegoceron.dev@outlook.com>
2024-05-21 13:06:28 -0600

    feat: :card_file_box: add database

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,12 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
 import vue from "@astrojs/vue";
-
 import auth from "auth-astro";
+
+import db from "@astrojs/db";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [mdx(), sitemap(), tailwind(), vue(), auth()]
+  integrations: [mdx(), sitemap(), tailwind(), vue(), auth(), db()]
 });
