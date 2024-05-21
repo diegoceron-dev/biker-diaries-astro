55449f3366de1ed56101d1d35d181583f8045876
Diego Ceron <diegoceron.dev@outlook.com>
2024-05-21 13:43:14 -0600

    feat: :sparkles: add vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,17 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import vue from "@astrojs/vue";
 import auth from "auth-astro";
-
 import db from "@astrojs/db";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [mdx(), sitemap(), tailwind(), vue(), auth(), db()]
+  output: "server",
+  integrations: [mdx(), sitemap(), tailwind(), vue(), auth(), db()],
+  adapter: vercel()
 });
