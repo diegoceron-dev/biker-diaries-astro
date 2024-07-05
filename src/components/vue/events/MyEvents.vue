b4f5d90c5460a85b9de643d8bb6fbe3cb6c04f37
Diego Ceron <diegoceron.dev@outlook.com>
2024-07-05 01:23:39 -0600

    fix: fix theme

--- src/components/vue/events/MyEvents.vue
+++ src/components/vue/events/MyEvents.vue
@@ -18,13 +18,13 @@ import {
 } from "@/components/ui/card";
 import { DateFormatter } from "@internationalized/date";
 import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
 
 const useServiceCatalogs = useCatalog();
 const useEvents = useEvent();
 
 const catalog = useStore(typeEventCatalogStore);
 const events = useStore(eventsStore);
-
 
 // Obtener datos al montar el componente
 onMounted(async () => {
@@ -45,22 +45,18 @@ const showDate = (item: Event) => {
 const showEventType = (item: Event) => {
   return `${catalog.value[item.eventType].description}`;
 };
+
+const goToEvents = () => {
+  window.location.href = "/events/create";
+};
 </script>
 
 <template>
   <div>
     <div class="flex flex-col space-y-4 py-12 px-24">
       <div class="flex flex-row justify-between">
-        <p class="text-2xl text-gray-700">Mis Eventos</p>
-        <div>
-          <a href="/events/create">
-            <button
-              class="w-full bg-primary text-white focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-lg px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800 transform transition duration-500 hover:scale-110 cursor-pointer"
-            >
-              Crear nuevo evento
-            </button>
-          </a>
-        </div>
+        <p class="text-2x">Mis Eventos</p>
+        <a href="/events/create"><Button>Crear nuevo evento</Button></a>
       </div>
       <div class="flex flex-col flex-grow overflow-y-auto">
         <div v-if="Object.keys(events).length === 0" class="text-gray-400">
